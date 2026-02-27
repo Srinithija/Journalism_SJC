3c9ad70117c002e70af802f508e0db804a629479
Srinithija <srinithijasivakumar@gmail.com>
2026-02-27 08:17:35 +0530

    deploy1

--- components/Homepage/AbcChannel.js
+++ components/Homepage/AbcChannel.js
@@ -1,11 +1,9 @@
 import Heading from "../Heading";
 import Canvas from "../Generic/Canvas";
-import { useTranslation } from "next-i18next";
 const AbcChannel = () => {
-  const { t } = useTranslation();
   return (
     <Canvas bgcolor="white">
-      <Heading title={t("homepage.abcChannel")} />
+      <Heading title={"ABC Channel"} />
       <div className="flex md:gap-y-8 md:my-4 p-6">
         <iframe
           className="aspect-video w-full h-auto"
